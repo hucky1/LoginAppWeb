ed2ec3384580412dd64e728e2a86146ce645d341
hucky1 <ashkredov41@gmail.com>
2021-05-26 16:41:00 +0300

    Added nlog!

--- LoginAppWeb/Controllers/LoginController.cs
+++ LoginAppWeb/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
-﻿using LoginAppWeb.Models;
+﻿    using LoginAppWeb.Models;
 using LoginAppWeb.Services.Business;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace LoginAppWeb.Controllers
 {
     public class LoginController : Controller
     {
+        private static Logger logger = LogManager.GetLogger("myAppLogerrule");
         // GET: Login
         public ActionResult Index()
         {
@@ -17,17 +19,29 @@ namespace LoginAppWeb.Controllers
         }
         public ActionResult Login(UserModel userModel)
         {
+            logger.Info("Entering the login controller. Login method");
+            try
+            {
+                SecurityService securityService = new SecurityService();
+                bool success = securityService.Authenticate(userModel);
+                if (success)
+                {
+                    logger.Info("Exit login controller. Login success.");
+                    return View("LoginSuccess", userModel);
+                }
+                else
+                {
+                    logger.Info("Exit login controller. Login failure!");
+                    return View("LoginFailure");
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.Error($"Exception {ex.Message}");
+                return Content($"Exception in login {ex.Message}");
+            }
             //return $"Results. Username = {userModel.Username}. Password = {userModel.Password}";
-            SecurityService securityService = new SecurityService();
-            bool success = securityService.Authenticate(userModel);
-            if (success)
-            {
-                return View("LoginSuccess",userModel);
-            }
-            else
-            {
-                return View("LoginFailure");
-            }
+           
         }
     }
 }
